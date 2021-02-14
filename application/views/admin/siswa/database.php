a3cf8892396e4b511f0e37fb450402a749c3feed
luqni <mluqnibaehaqi@gmail.com>
2021-02-15 00:25:36 +0700

    n

--- application/views/admin/siswa/database.php
+++ application/views/admin/siswa/database.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_USER','user');
+define('DB_PASS','P@ssw0rd');
 define('DB_NAME','inventory');
 
 $db_conn = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
